22add387812809e80b12349e23941f0f9387c327
Dhruvi kakadiya <kakadiyadhruvi1700@gmail.com>
2021-03-18 17:16:22 +0530

    Add step2 file

---
+++ src/component/Interviews/Steps/Step2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+const Step2 = ()=>{
+    return(
+        <>
+            <div className="step2">
+                <h4>MANAGER</h4>
+                <TextField id="outlined-basic" placeholder="Manager Name" variant="outlined" />
+
+            </div>
+        </>
+    );
+}
+export default Step2;
