24d13eb2d94494fd7b3736bfe217afaf6464f346
chandblog <chandanikhanehsa@gamil.com>
2021-03-19 17:24:42 +0530

    "khabar nai"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //import CompanyProfilePage from './component/Companyprofile/CompanyProfilePage'
 import Home from './component/Home/Home'
-import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
-import Team from './component/Team/Team'
+// import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+// import Team from './component/Team/Team'
 
 // import Team from './component/Team/Team'
-import AppD from './component/Interviews/AppD'
-import Navbar  from './component/Navbar/Navbar'
+// import AppD from './component/Interviews/AppD'
+// import Navbar  from './component/Navbar/Navbar'
 
 function App() {
   return (
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
-import '../Home/home.css'
+import './home.css'
 
 import img1 from '../img/About/Admininterview.png'
 
