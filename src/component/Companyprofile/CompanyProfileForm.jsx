1a3b2938506a6b0b0382d45086f251df7c7d5545
Dhruvikakadiya <kakadiyadhruvi1700@gmail.com>
2021-03-19 13:49:01 +0530

    change all conflict

--- src/component/Companyprofile/CompanyProfileForm.jsx
+++ src/component/Companyprofile/CompanyProfileForm.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-<<<<<<< HEAD:src/component/Company_profile/Company_profile_form.jsx
-import Company_information from './Company_information'
-=======
 import CompanyInformation from './CompanyInformation'
->>>>>>> e9207bfa855188ca9ff110f3c445ba5268807bca:src/component/Companyprofile/CompanyProfileForm.jsx
 import Department from './Department'
 import JobTitle from './JobTitle'
 import Manager from './Manager'
-<<<<<<< HEAD:src/component/Company_profile/Company_profile_form.jsx
-import JobTitle from './JobTitle'
-import  './Company.css';
-=======
->>>>>>> e9207bfa855188ca9ff110f3c445ba5268807bca:src/component/Companyprofile/CompanyProfileForm.jsx
 
 const CompanyProfileForm=()=> {
     return (
@@ -20,11 +11,7 @@ const CompanyProfileForm=()=> {
         <div className="header2">
         <h4>Comapany Information</h4>
         </div>
-<<<<<<< HEAD:src/component/Company_profile/Company_profile_form.jsx
-            <Company_information/>
-=======
             <CompanyInformation/>
->>>>>>> e9207bfa855188ca9ff110f3c445ba5268807bca:src/component/Companyprofile/CompanyProfileForm.jsx
             <Department/>
             <Manager/>
             <JobTitle/>
