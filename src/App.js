1956ead6860c1a6c653f1753701cd8b6e33f89ce
chandblog <chandanikhanehsa@gamil.com>
2021-03-19 12:41:35 +0530

    change in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 // import Company_Profile_page from './component/Company_profile/Company_Profile_page'
 // import Home from './component/Home/Home'
 
 import Team from './component/Team/Team'
-=======
-
-import Company_Profile_page from './component/Company_profile/Company_Profile_page';
->>>>>>> 5dffd15a802b16c26a85d4230c540b8be15f512c
 
 function App() {
   return (
    <>
-<<<<<<< HEAD
    {/* <Home/> */}
   {/* <Company_Profile_page/> */}
    <Team/>
   
-=======
-   <Company_Profile_page/>
->>>>>>> 5dffd15a802b16c26a85d4230c540b8be15f512c
    </>
   );
 }
--- src/component/Companyprofile/CompanyProfileForm.jsx
+++ src/component/Companyprofile/CompanyProfileForm.jsx
@@ -3,11 +3,8 @@ import Company_information from './Company_information'
 import Department from './Department'
 import JobTitle from './JobTitle'
 import Manager from './Manager'
-<<<<<<< HEAD:src/component/Company_profile/Company_profile_form.jsx
 import JobTitle from './JobTitle'
 import  '../Company_profile/Company.css';
-=======
->>>>>>> 5dffd15a802b16c26a85d4230c540b8be15f512c:src/component/Companyprofile/CompanyProfileForm.jsx
 
 const Company_profile_form=()=> {
     return (
