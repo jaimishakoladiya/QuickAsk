8b5517f0235f1e14404338c5008f9c7abe071a49
chandblog <chandanikhanehsa@gamil.com>
2021-03-19 14:16:03 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 // import Company_Profile_page from './component/Company_profile/Company_Profile_page'
 // import Home from './component/Home/Home'
 
-<<<<<<< HEAD
-import CreateInterview from './component/Interviews/CreateInterview'
-import CandidateDetails from './component/Interviews/CandidateDetails';
-
-import './component/Interviews/Interviews.css';
-
-=======
 import Team from './component/Team/Team'
->>>>>>> chandani
 
 function App() {
   return (
    <>
-<<<<<<< HEAD
-      <CreateInterview/>
-      <CandidateDetails/>
-      
-   
-=======
    {/* <Home/> */}
   {/* <Company_Profile_page/> */}
    <Team/>
   
->>>>>>> chandani
    </>
   );
 }
