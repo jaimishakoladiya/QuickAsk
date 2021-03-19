2033aedaadfd2ae962fbcb6c0edbb8653ad01a11
chandblog <chandanikhanehsa@gamil.com>
2021-03-19 15:16:28 +0530

    run new file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 // import Company_Profile_page from './component/Company_profile/Company_Profile_page'
 // import Home from './component/Home/Home'
 
-import Team from './component/Team/Team'
-<<<<<<< HEAD
-=======
+// import Team from './component/Team/Team'
 import AppD from './component/Interviews/AppD'
->>>>>>> 144dcc4341fb9936a9dde114fd1d551278dfa0ed
 
 function App() {
   return (
    <>
    {/* <Home/> */}
   {/* <Company_Profile_page/> */}
-<<<<<<< HEAD
-   <Team/>
-  
-=======
    {/* <Team/> */}
   <AppD/>
->>>>>>> 144dcc4341fb9936a9dde114fd1d551278dfa0ed
    </>
   );
 }
