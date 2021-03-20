1b3fdb7018378a843adf6b8abf872522d5f81bb8
chandblog <chandanikhanehsa@gamil.com>
2021-03-20 17:01:52 +0530

    "change contact page"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 //import CompanyProfilePage from './component/Companyprofile/CompanyProfilePage'
 // import Home from './component/Home/Home'
- import {BrowserRouter} from 'react-router-dom';
-// import Team from './component/Team/Team'
+//  import {BrowserRouter} from 'react-router-dom';
+import Team from './component/team/Team'
 
-import Home from './component/home/Home'
+// import Home from './component/home/Home'
 
-import HomeNavbarRouter from './component/navbar/HomeNavbarRouter';
+// import HomeNavbarRouter from './component/navbar/HomeNavbarRouter';
 
 function App() {
   return (
    <>
-   <BrowserRouter>
+   {/* <BrowserRouter>
      <HomeNavbarRouter/>
-   </BrowserRouter>
+   </BrowserRouter> */}
 
     {/* <Home/> */}
+    <Team/>
    </>
   );
 }
--- src/component/Team/Contact/Contact.js
+++ src/component/Team/Contact/Contact.js
@@ -1,36 +1,54 @@
 import React from 'react'
 import img1 from '../contact/admin.png'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import '../contact/contact.css'
-function Contact() {
+import { responsiveFontSizes } from '@material-ui/core';
+
+const Contact=()=>{
+    const field={ 
+        margin:"2px",
+        marginTop:"20px",
+    width:600,
+        }
+       
+
+
 return (
-<body className="main">
-<div className="container">
-<div className="formbox">
+<body className="contact-main">
+<div className="contact-container">
+<div className="contact-formbox">
 <form>
-<h2>Contact Us</h2>
-<h3>As A New StartUp , We Want To know Your Comments..</h3>
-<div className="inputbox">
-<input type="text" name="" required/>
-<span>Full Name</span>
-</div>
-<div className="inputbox">
+<h4 className="contact-title">CONTACT US</h4>
+<h3 className="contact-text">As A New StartUp , We Want To know Your Comments..</h3>
+<TextField style={field} id="standard-basic" label="FirstName" variant="standard" />
+
+<TextField style={field} id="standard-basic" label="LastName" variant="standard" />
+<TextField style={field} id="standard-basic" label="Subject" variant="standard" />
+<TextField style={field} id="standard-basic" label="Email" variant="standard" />
+<TextField style={field} id="standard-textarea" label="Comment.. " multiline   rowsMax={5}/><br></br><br></br><br></br>
+<Button className="contact-butn" variant="contained" color="secondary">
+        Submit
+      </Button><br/>
+{/* <div className="contact-inputbox">
 <input type="text" name="" required/>
 <span>Subject</span>
 </div>
-<div className="inputbox">
+<div className="contact-inputbox">
 <input type="text" name="" required/>
 <span>Email</span>
 </div>
-<div className="inputbox">
+<div className="contact-inputbox">
 <textarea name="" required></textarea>
 <span>Comment..</span>
 </div>
-<div className="inputbox">
+
+<div className="contact-inputbox">
 <input type="submit" value="Submit" name=""/>
-</div>
+</div> */}
 </form>
 </div>
-<div className="img">
+<div className="contact-img">
 <img src={img1}/>
 </div>
 
