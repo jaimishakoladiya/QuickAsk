9fea4d06aebfd0f706d0e7ddc6b01567ba6e1870
jhkblog <jemishakoladiya2001@gmail.com>
2021-03-20 11:45:28 +0530

    Homenavbar router created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 //import CompanyProfilePage from './component/Companyprofile/CompanyProfilePage'
 import Home from './component/Home/Home'
-import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import Team from './component/Team/Team'
 
 // import Team from './component/Team/Team'
 import AppD from './component/Interviews/AppD'
-import Navbar  from './component/Navbar/Navbar'
+
+import HomeNavbarRouter from './component/Navbar/HomeNavbarRouter';
 
 function App() {
   return (
    <>
-   {/* <Router>
-     <Switch>
-     <Navbar/>
-     </Switch>
-   </Router> */}
+   <BrowserRouter>
+     <HomeNavbarRouter/>
+   </BrowserRouter>
 
-    <Home/> 
-  {/* <CompanyProfilePage/>  */}
-   {/* <Team/>  */}
-  {/* <AppD/>  */}
- 
+    
    </>
   );
 }
---
+++ src/component/Navbar/HomeNavbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+// import "./index.css";
+
+
+const HomeNavbar =()=>{
+   return( <>
+   <div className="header">
+    <NavLink to="/" className="navlink">home  </NavLink>
+    <NavLink to="/About" className="navlink">About </NavLink>
+    <NavLink to="/CompanyProfilePage" className="navlink">Comapny Profile </NavLink>
+    <NavLink to="/CompanyProfilePage" className="navlink">Comapny Profile </NavLink>
+    </div>
+    </>
+   )
+}
+
+export default  HomeNavbar;
