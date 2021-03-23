7ec127b6ec021dd6894016db9a9033d5c75134c7
Dhruvikakadiya <kakadiyadhruvi1700@gmail.com>
2021-03-23 16:16:45 +0530

    add Button com,ponent

--- src/component/Interviews/AppD.jsx
+++ src/component/Interviews/AppD.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import ButtonDelete from './Steps/ButtonDelete'
 import CandidateDetails from './CandidateDetails';
 import CreateInterview from './CreateInterview';
 
 const AppD = ()=>{
     return(<>
-             <CreateInterview/>
-             <CandidateDetails/>
              
+             <CandidateDetails/>
+            {/* <CreateInterview/> */}
             
         </>
     );
